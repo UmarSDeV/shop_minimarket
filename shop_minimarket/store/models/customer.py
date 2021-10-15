cd4e1cd3198064ec7714935e85eb9eb1e2f021e1
UmarSDeV <87483387+UmarSDeV@users.noreply.github.com>
2021-10-15 23:37:42 +0500

    Shop minimarket

---
+++ shop_minimarket/store/models/customer.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def register(self):
+        self.save()
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email=email)
+        except:
+            return False
+
+    def isExists(self):
+        if Customer.ocjects.filter(email=self.email):
+            return True
+        return False
